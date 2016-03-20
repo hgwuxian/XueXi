44a5d9874773b0cf359e278f46d42a9541ca8dd9
hgwuxian <hgwuxian@outlook.com>
2016-03-20 22:31:41 +0800

    optimization ListView
    利用 ListView 的缓存模式，避免重复findViewById

--- app/src/main/java/com/hu_yao/xuexi/MainActivityFragment.java
+++ app/src/main/java/com/hu_yao/xuexi/MainActivityFragment.java
@@ -7,10 +7,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.zip.Inflater;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -39,12 +41,28 @@ public class MainActivityFragment extends Fragment {
         //使用 ArrayAdapter ，并 public ArrayAdapter (Context context, int resource, int textViewResourceId, List<T> objects)
         arrayAdapter = new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textview,weekForecast);
         //View赋予位置
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        //利用 ListView 的缓存模式
+        ViewHorld viewHorld = null;
+        if (container == null) {
+            viewHorld = new ViewHorld();
+            container = (ViewGroup) inflater.inflate(R.layout.fragment_main,null);
+            //在 viewHorld 需找ListView
+            viewHorld.listView = (ListView) container.findViewById(R.id.listview_frecast);
+            //通过setTag将container View 与 viewHorld 进行绑定，将重复findViewById的控件放入viewHorld
+            container.setTag(viewHorld);
+        }else {
+            //通过 viewHorld 找到对应控件，避免重复findViewById
+            viewHorld = (ViewHorld) container.getTag();
+        }
         //需找ListView
-        ListView listView = (ListView) rootView.findViewById(R.id.listview_frecast);
+//        ListView listView = (ListView) container.findViewById(R.id.listview_frecast);
         //加载
-        listView.setAdapter(arrayAdapter);
+        viewHorld.listView.setAdapter(arrayAdapter);
         //返回View
-        return rootView;
+        return container;
+    }
+    //创建类，并初始化
+    class ViewHorld {
+        public ListView listView;
     }
 }
