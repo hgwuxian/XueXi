155746b8f248c91ad0e6944b6029d4bb85eefbe9
hgwuxian <hgwuxian@outlook.com>
2016-04-03 20:43:43 +0800

    增加从网上获取天气
    增加API，上网权限，分开线程，加载到ListView

--- app/src/main/java/com/hu_yao/xuexi/MainActivity.java
+++ app/src/main/java/com/hu_yao/xuexi/MainActivity.java
@@ -1,14 +1,10 @@
 package com.hu_yao.xuexi;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
 
 
--- app/src/main/java/com/hu_yao/xuexi/MainActivityFragment.java
+++ app/src/main/java/com/hu_yao/xuexi/MainActivityFragment.java
@@ -1,33 +1,76 @@
 package com.hu_yao.xuexi;
 
-import android.support.v4.app.Fragment;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.format.Time;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.zip.Inflater;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
-    //初始化 ArrayAdapter 并附名
-    private  ArrayAdapter<String> arrayAdapter;
+    private ArrayAdapter<String> mForecastAdapter;
+
     public MainActivityFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //处理 menu 事件，调用 onCreateOptionsMenu （true）表示显示
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        //位置
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    //点击 refresh 会发生什么
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        //当点击发生时返回 true
+        if (id == R.id.action_refresh) {
+            FetchWeatherTask weatherTask = new FetchWeatherTask();
+            weatherTask.execute("Chongqing");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        String [] zhi = {
+        String[] zhi = {
                 "Mon 6/23 - Sunny - 31/17",
                 "Tue 6/24 - Foggy - 21/8",
                 "Wed 6/25 - Cloudy - 22/17",
@@ -36,33 +79,270 @@ public class MainActivityFragment extends Fragment {
                 "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
                 "Sun 6/29 - Sunny - 20/7"
         };
+//        //将数组赋予为List
+//        List<String> weekForecast = new ArrayList<>(Arrays.asList(zhi));
+//        //使用 ArrayAdapter ，并 public ArrayAdapter (Context context, int resource, int textViewResourceId, List<T> objects)
+//        ArrayAdapter<String> mForecastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
+//        //View赋予位置
+//        //利用 ListView 的缓存模式
+//        ViewHorld viewHorld ;
+//        if (container == null) {
+//            viewHorld = new ViewHorld();
+//            container = (ViewGroup) inflater.inflate(R.layout.fragment_main, null);
+//            //在 viewHorld 需找ListView
+//            viewHorld.listView = (ListView) container.findViewById(R.id.listview_frecast);
+//            //通过setTag将container View 与 viewHorld 进行绑定，将重复findViewById的控件放入viewHorld
+//            container.setTag(viewHorld);
+//        } else {
+//            //通过 viewHorld 找到对应控件，避免重复findViewById
+//            viewHorld = (ViewHorld) container.getTag();
+//        }
+//        //需找ListView
+//        ListView listView = (ListView) container.findViewById(R.id.listview_frecast);
+//        //加载
+//        viewHorld.listView.setAdapter(mForecastAdapter);
+//        //返回View
+//        return container;
+//    }
+//
+//    //创建类，并初始化
+//    class ViewHorld {
+//        public ListView listView;
+//    }
         //将数组赋予为List
         List<String> weekForecast = new ArrayList<String>(Arrays.asList(zhi));
-        //使用 ArrayAdapter ，并 public ArrayAdapter (Context context, int resource, int textViewResourceId, List<T> objects)
-        arrayAdapter = new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textview,weekForecast);
-        //View赋予位置
-        //利用 ListView 的缓存模式
-        ViewHorld viewHorld = null;
-        if (container == null) {
-            viewHorld = new ViewHorld();
-            container = (ViewGroup) inflater.inflate(R.layout.fragment_main,null);
-            //在 viewHorld 需找ListView
-            viewHorld.listView = (ListView) container.findViewById(R.id.listview_frecast);
-            //通过setTag将container View 与 viewHorld 进行绑定，将重复findViewById的控件放入viewHorld
-            container.setTag(viewHorld);
-        }else {
-            //通过 viewHorld 找到对应控件，避免重复findViewById
-            viewHorld = (ViewHorld) container.getTag();
-        }
-        //需找ListView
-//        ListView listView = (ListView) container.findViewById(R.id.listview_frecast);
+
+        // 使用 ArrayAdapter ，并 public ArrayAdapter (Context context, int resource, int textViewResourceId, List<T> objects)
+        mForecastAdapter =
+                new ArrayAdapter<String>(
+                        getActivity(), // The current context (this activity)
+                        R.layout.list_item_forecast, // The name of the layout ID.
+                        R.id.list_item_forecast_textview, // The ID of the textview to populate.
+                        weekForecast);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        // 需找ListView
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_frecast);
         //加载
-        viewHorld.listView.setAdapter(arrayAdapter);
-        //返回View
-        return container;
+        listView.setAdapter(mForecastAdapter);
+        return rootView;
     }
-    //创建类，并初始化
-    class ViewHorld {
-        public ListView listView;
+    class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+
+        /* The date/time conversion code is going to be moved outside the asynctask later,
+         * so for convenience we're breaking it out into its own method now.
+         */
+        private String getReadableDateString(long time) {
+            // 由于API返回一个UNIX时间（以秒为单位），
+            // 它必须被转换为毫秒，以便被转换为有效日期。
+            SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
+            return shortenedDateFormat.format(time);
+        }
+
+        /**
+         * 准备天气的 high/lows 演示
+         */
+        private String formatHighLows(double high, double low) {
+            // Math.round () 控制其中数字，返回四舍五入参数为整数的结果
+            long roundedHigh = Math.round(high);
+            long roundedLow = Math.round(low);
+
+            return roundedHigh + "/" + roundedLow;
+        }
+
+        /**
+         * Take the String representing the complete forecast in JSON Format and
+         * pull out the data we need to construct the Strings needed for the wireframes.
+         * <p/>
+         * Fortunately parsing is easy:  constructor takes the JSON string and converts it
+         * into an Object hierarchy for us.
+         */
+        private String[] getWeatherDataFromJson(String forecastJsonStr, int numDays)
+                throws JSONException {
+
+            // 这些是需要被提取的JSON对象的名称
+            final String OWM_LIST = "list";
+            final String OWM_WEATHER = "weather";
+            final String OWM_TEMPERATURE = "temp";
+            final String OWM_MAX = "max";
+            final String OWM_MIN = "min";
+            final String OWM_DESCRIPTION = "main";
+
+            JSONObject forecastJson = new JSONObject(forecastJsonStr);
+            JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
+
+            // OWM returns daily forecasts based upon the local time of the city that is being
+            // asked for, which means that we need to know the GMT offset to translate this data
+            // properly.
+
+            // Since this data is also sent in-order and the first day is always the
+            // current day, we're going to take advantage of that to get a nice
+            // normalized UTC date for all of our weather.
+            // setToNow() 设置给定的时间对象到当前时间的时间
+
+            Time dayTime = new Time();
+            dayTime.setToNow();
+
+            // we start at the day returned by local time. Otherwise this is a mess.（我们开始在本地时间返回的一天。否则，这是一个烂摊子）
+            int julianStartDay = Time.getJulianDay(System.currentTimeMillis(), dayTime.gmtoff);
+
+            // now we work exclusively in UTC
+            dayTime = new Time();
+
+            String[] resultStrs = new String[numDays];
+            for (int i = 0; i < weatherArray.length(); i++) {
+                // For now, using the format "Day, description, hi/low"
+                String day;
+                String description;
+                String highAndLow;
+
+                // Get the JSON object representing（代表） the day
+                JSONObject dayForecast = weatherArray.getJSONObject(i);
+
+                // The date/time is returned as a long.  We need to ‘convert that’（转换他）
+                // into something human-readable, since most people won't read "1400356800" as（转换时间，变成可读）
+                // "this saturday".
+                long dateTime;
+                // Cheating to convert this to UTC time, which is what we want anyhow
+                dateTime = dayTime.setJulianDay(julianStartDay + i);
+                day = getReadableDateString(dateTime);
+
+                // description is in a child array called "weather", which is 1 element long.(显示当天（时间）天气情况)
+                JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
+                description = weatherObject.getString(OWM_DESCRIPTION);
+
+                // Temperatures are in a child object called "temp".  Try not to name variables（temp 表示温度）
+                // "temp" when working with temperature.  It confuses everybody.
+                JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
+                double high = temperatureObject.getDouble(OWM_MAX);
+                double low = temperatureObject.getDouble(OWM_MIN);
+
+                highAndLow = formatHighLows(high, low);
+                resultStrs[i] = day + " - " + description + " - " + highAndLow;
+            }
+
+            for (String s : resultStrs) {
+                Log.v(LOG_TAG, "Forecast entry: " + s);
+            }
+            return resultStrs;
+
+        }
+
+
+
+        @Override
+        protected String[] doInBackground(String... params) {
+            //如果没有给定位置，就输出空（毕竟没什么可显示ㄟ( ▔, ▔ )ㄏ ）
+            if (params.length == 0) {
+                return null;
+            }
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+
+            String format = "json";
+            String units = "metric";
+            int numDays = 7;
+            String appid = "94b6850414070e88fa9f8f716ce7afc1";
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+//                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=Chongqing&mode=json&units=metric&cnt=7&APPID=94b6850414070e88fa9f8f716ce7afc1");
+
+                //分开重新组合 URL
+                final String FORECAST_BASE_URL =
+                        "http://api.openweathermap.org/data/2.5/forecast/daily?";
+                final String QUERY_PARAM = "q";
+                final String FORMAT_PARAM = "mode";
+                final String UNITS_PARAM = "units";
+                final String DAYS_PARAM = "cnt";
+                final String APPID_PARAM = "APPID";
+
+                Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                        .appendQueryParameter(QUERY_PARAM, params[0])
+                        .appendQueryParameter(FORMAT_PARAM, format)
+                        .appendQueryParameter(UNITS_PARAM, units)
+                        .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                        .appendQueryParameter(APPID_PARAM, appid)
+                        .build();
+
+                URL url = new URL(builtUri.toString());
+
+                Log.v(LOG_TAG, "Built URI " + builtUri.toString());
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuilder buffer = new StringBuilder();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line).append("\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+            try {
+                return getWeatherDataFromJson(forecastJsonStr, numDays);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+
+            // 返回 null，仅仅发生错误或 分析预测.
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(String[] result) {
+            if (result != null) {
+                mForecastAdapter.clear();
+                for(String dayForecastStr : result) {
+                    mForecastAdapter.add(dayForecastStr);
+                }
+                // New data is back from the server.  Hooray!
+            }
+            super.onPostExecute(result);
+        }
     }
 }
