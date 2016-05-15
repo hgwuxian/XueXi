3f015cf6de3faed8aeb51fad77da30d25aa3c2aa
hgwuxian <hgwuxian@outlook.com>
2016-05-15 19:19:21 +0800

    温度单位设置，

--- app/src/main/java/com/hu_yao/xuexi/MainActivityFragment.java
+++ app/src/main/java/com/hu_yao/xuexi/MainActivityFragment.java
@@ -158,8 +158,8 @@ public class MainActivityFragment extends Fragment {
         private String formatHighLows(double high, double low,String unitType) {
 
             if(unitType.equals(getString(R.string.pref_units_imperial))){
-                high = (high*1.8)+ 32;
-                low = (low *1.8) +32 ;
+                high = (high *1.8)+ 32;
+                low = (low * 1.8) +32 ;
             }else if (!unitType.equals(getString(R.string.pref_units_metric))){
                 Log.d(LOG_TAG, "Unit type not found: " + unitType);
             }
@@ -209,16 +209,16 @@ public class MainActivityFragment extends Fragment {
 
             // now we work exclusively in UTC
             dayTime = new Time();
+            String[] resultStrs = new String[numDays];
 
             //数据默认是在 metric
             //我们一开始就在数据库中存取一个值
 
-            String[] resultStrs = new String[numDays];
+
             SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
             String unitType = sharedPreferences.getString(
                     getString(R.string.pref_units_key),
-                    getString(R.string.pref_units_metric)
-            );
+                    getString(R.string.pref_units_metric));
             for (int i = 0; i < weatherArray.length(); i++) {
                 // For now, using the format "Day, description, hi/low"
                 String day;
